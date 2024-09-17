d4653075a2a92ed60fca1d7f32aa3c846b2571a2
UO295808 <UO295808@EII-L04-07.aulasuo.uniovi.es>
2024-09-17 12:06:06 +0200

    Changed number and print message

--- src/ips/Main.java
+++ src/ips/Main.java
@@ -2,9 +2,9 @@ package ips;
 
 public class Main {
 	public static void main(String[] args) {
-		int i = 35;
+		int i = 3;
 		int j = 1986;
-		System.out.println(i);
+		System.out.println("The number is: " + i);
 		System.out.println(j);
 	}
 }
